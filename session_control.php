b6adb91cb7581351cef85617b2f99fc1f847ca88
Shreshtha das <54697366+Madmaxscorpion@users.noreply.github.com>
2023-04-13 14:37:34 +0530

    Update session_control.php

--- session_control.php
+++ session_control.php
@@ -3,7 +3,7 @@
 // WebVulScan
 // - Web Application Vulnerability Scanning Software
 //
-// Copyright (C) 2012 Dermot Blair (webvulscan@gmail.com)
+// Copyright (C) 2023 The Technopaths (technopaths@gmail.com)
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -29,10 +29,8 @@
 // - TCPDF(http://www.tcpdf.org/) - Licensed under GNU Lesser General Public 
 //   License Version 3
 // - jQuery(http://jquery.com/) - Dual licensed the MIT or GNU General Public
-//   License Version 2 licenses
-// - Calliope(http://www.towfiqi.com/xhtml-template-calliope.html) - 
+//   License Version 2 licenses 
 //   Licensed under the Creative Commons Attribution 3.0 Unported License 
-//
 // This software was developed, and should only be used, entirely for 
 // ethical purposes. Running security testing tools such as this on a 
 // website(web application) could damage it. In order to stay ethical, 
