367595c6a4a1e97447fefb94a9861bc074aaaf86
Shreshtha das <54697366+Madmaxscorpion@users.noreply.github.com>
2024-06-04 10:10:34 +0530

    Update webvulscan.sql

--- webvulscan.sql
+++ webvulscan.sql
@@ -1,23 +1,27 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.9
--- http://www.phpmyadmin.net
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 18, 2012 at 06:08 
--- Server version: 5.5.8
--- PHP Version: 5.3.5
+-- Generation Time: May 12, 2024 at 12:07 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `webvulscan`
 --
+CREATE DATABASE IF NOT EXISTS `webvulscan` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `webvulscan`;
 
 -- --------------------------------------------------------
 
@@ -25,6 +29,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Table structure for table `tests`
 --
 
+DROP TABLE IF EXISTS `tests`;
 CREATE TABLE IF NOT EXISTS `tests` (
   `id` int(11) NOT NULL,
   `status` text NOT NULL,
@@ -72,12 +77,7 @@ CREATE TABLE IF NOT EXISTS `tests` (
   KEY `start_timestamp_7` (`start_timestamp`),
   KEY `finish_timestamp_7` (`finish_timestamp`),
   KEY `scan_finished_4` (`scan_finished`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tests`
---
-
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -85,6 +85,7 @@ CREATE TABLE IF NOT EXISTS `tests` (
 -- Table structure for table `test_results`
 --
 
+DROP TABLE IF EXISTS `test_results`;
 CREATE TABLE IF NOT EXISTS `test_results` (
   `test_id` int(11) NOT NULL,
   `type` text NOT NULL,
@@ -95,12 +96,7 @@ CREATE TABLE IF NOT EXISTS `test_results` (
   KEY `test_id_2` (`test_id`),
   KEY `test_id_3` (`test_id`),
   KEY `test_id_4` (`test_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `test_results`
---
-
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -108,16 +104,19 @@ CREATE TABLE IF NOT EXISTS `test_results` (
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `username` text NOT NULL,
   `password` text NOT NULL,
   `email` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `users`
 --
 
+INSERT INTO `users` (`username`, `password`, `email`) VALUES
+('Soumwadeep', '7c222fb2927d828af22f592134e8932480637c0d', 'sguha@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -125,6 +124,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Table structure for table `vulnerabilities`
 --
 
+DROP TABLE IF EXISTS `vulnerabilities`;
 CREATE TABLE IF NOT EXISTS `vulnerabilities` (
   `id` text NOT NULL,
   `name` text NOT NULL,
@@ -139,20 +139,25 @@ CREATE TABLE IF NOT EXISTS `vulnerabilities` (
   KEY `priority_num_2` (`priority_num`),
   KEY `priority_num_4` (`priority_num`),
   KEY `priority_num_6` (`priority_num`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `vulnerabilities`
 --
 
 INSERT INTO `vulnerabilities` (`id`, `name`, `description`, `solution`, `priority`, `priority_num`) VALUES
-('autoc', 'Autocomplete not disabled on sensitive input fields', 'Autocomplete occurs when the browser caches data, such as a user\\''s username and password for an application, so the user will not have to enter them any time they access the application. Forms that process sensitive data such as passwords should always have autocomplete disabled. If an attacker gains access to a user\\''s browser cache, he could easily obtain the sensitive information which may be saved in plaintext.', 'Disable the autocomplete attribute of input fields that hold sensitive data. This can be done by placing autocomplete=\\"off\\" inside the tags of the input field. You can also disable autocomplete for an entire form by placing it inside the tags of the form.', 'Low', 1),
+('autoc', 'Autocomplete not disabled on sensitive input fields', 'Autocomplete occurs when the browser caches data, such as a user\\\'s username and password for an application, so the user will not have to enter them any time they access the application. Forms that process sensitive data such as passwords should always have autocomplete disabled. If an attacker gains access to a user\\\'s browser cache, he could easily obtain the sensitive information which may be saved in plaintext.', 'Disable the autocomplete attribute of input fields that hold sensitive data. This can be done by placing autocomplete=\\\"off\\\" inside the tags of the input field. You can also disable autocomplete for an entire form by placing it inside the tags of the form.', 'Low', 1),
 ('bannerdis', 'HTTP Banner Disclosure', 'The application discloses information about the technologies used such as the web server, operating system, cryptography tools, or programming languages. An attacker could identify vulnerabilities in these technologies and use them to exploit the server, therefore, potentially exploiting the application.', 'You can disable the server from disclosing this information to users. This is typically done by editing the configuration files of the various technologies and then restarting the system.', 'Medium', 2),
-('sslcert', 'SSL certificate is not trusted', 'The web application is using a SSL certificate which has been checked against Mozilla\\''s bundle of X.509 certificates of public Certificate Authorities and cannot be found. Therefore, the certificate cannot be validated and is not trusted.', 'Ensure the SSL certificate has been issued by a trusted authority.', 'Medium', 3),
+('sslcert', 'SSL certificate is not trusted', 'The web application is using a SSL certificate which has been checked against Mozilla\\\'s bundle of X.509 certificates of public Certificate Authorities and cannot be found. Therefore, the certificate cannot be validated and is not trusted.', 'Ensure the SSL certificate has been issued by a trusted authority.', 'Medium', 3),
 ('idor', '(Potentially Insecure) Direct Object References', 'Exposing a reference to an internal implementation object is known as a direct object reference. An example of an internal implementation object would be a file, directory, or database key. If the reference can be edited by a user and sufficient control is not in place, the user could manipulate the reference and possibly access unauthorized resources. <br><br>\r\nFor example, a URL like the following is exposing a direct object reference: http://www.example.com/displayFile.php?file=stats.txt. However, a malicious user could replace the file name and re-request the URL again in order to try and obtain system passwords. For example: http://www.example.com/displayFile.php?file=/../../../../etc/passwd<br><br>\r\nAutomated tools cannot typically identify such flaws as they cannot recognize what requires protection and what is safe or unsafe. Therefore, this result is only indicating that values which look like direct object references are exposed and they may be insecure.\r\n', 'Use indirect object references. For example, using the above scenario, pass an integer to the URL and this could be mapped to a file name once the request is made. If direct object references must be used, have an access control check in place to ensure the user is authorized to view the requested resource.', 'High', 4),
 ('unredir', 'Unvalidated Redirects', 'The application is redirecting the user to a page based on user-controllable data that is not validated correctly. An example of this would be a link that requests the following URL: http://www.example.com?redirect.php?redirect=user_can_replace_this.html. This could be edited to http://www.example.com?redirect.php?redirect=http://www.malicous-site.com, and if not validated correctly, it will redirect to the malicious site. Links like the latter could then be emailed to potential victims and they may click on them with no hesitation as www.example.com may be a trusted domain.', 'The target site that the user is being redirected to should not be exposed. If there is no way around this or it is simply too much effort to edit this design, ensure the user-controllable data is validated before redirecting to it. One countermeasure is to maintain a list of safe URLs that can be redirected to and check the user-controllable value against this list before performing the redirect. <br><br>\r\nAnother good countermeasure is to pass an integer to the URL that is redirecting. For example, http://www.example.com?redirect.php?redirect=3. This integer acts as an array index and an array of safe URLs is maintained by the web applications. ', 'High', 5),
-('dirlist', 'Directory Listing Enabled', 'The contents of one or more directories can be viewed by web users. Therefore, when a user requests a directory such as http://www.example.com/directoryname/ using their browser, a list of all files and directories contained in the requested directory will be displayed to the user. <br><br>\r\nThis could possibly expose sensitive information such as executables, text files, documentation, and installation and configuration files. An attacker could use these to map out the server\\''s directory structure and identify potentially vulnerable files or applications.', 'This can be a high risk vulnerability. This is typically enabled in the server\\''s configuration file but can sometimes arise from a vulnerability in particular applications. You can eliminate this vulnerability by disabling directory listing in the server\\''s configuration file and restart the server. The location and name of this file differs depending on what web server you are using.', 'High', 6),
+('dirlist', 'Directory Listing Enabled', 'The contents of one or more directories can be viewed by web users. Therefore, when a user requests a directory such as http://www.example.com/directoryname/ using their browser, a list of all files and directories contained in the requested directory will be displayed to the user. <br><br>\r\nThis could possibly expose sensitive information such as executables, text files, documentation, and installation and configuration files. An attacker could use these to map out the server\\\'s directory structure and identify potentially vulnerable files or applications.', 'This can be a high risk vulnerability. This is typically enabled in the server\\\'s configuration file but can sometimes arise from a vulnerability in particular applications. You can eliminate this vulnerability by disabling directory listing in the server\\\'s configuration file and restart the server. The location and name of this file differs depending on what web server you are using.', 'High', 6),
 ('rxss', 'Reflected Cross-Site Scripting', 'Cross-Site Scripting (XSS) is a vulnerability that allows code to be injected into the web application and viewed by users. The code may include HTML, JavaScript, or other client-side languages. Reflected XSS is a variation of XSS where user-controllable data is displayed back to the user, in the HTTP response of the request used for the attack, without being validated correctly.', 'This can be a high risk vulnerability and can be underestimated.  Mitigating this vulnerability uses a two-fold approach. Ensure all user-controllable data is validated after it is inputted and again before it is outputted to users. <br><br>\r\nBlacklisting is an approach which consists of checking the input data for malicious characters but a more effective approach is whitelisting. Whitelisting consists of only allowing certain characters to be submitted. For example checking if data submitted is alphanumeric and rejecting the request if it is not. You can use an approach like this after data is submitted and then perform a similar approach before data is outputted to the user. ', 'High', 8),
 ('sxss', 'Stored Cross-Site Scripting', 'Cross-Site Scripting (XSS) is a vulnerability that allows code to be injected into the web application and viewed by other users. The code may include HTML, JavaScript, or other client-side languages. Stored XSS is a variation of XSS where user-controllable data is stored and displayed to users at a later stage. For example, product reviews or posts on a forum.', 'This is a high risk vulnerability.  Mitigating this vulnerability uses a two-fold approach. Ensure all user-controllable data is validated before it is stored and again before it is outputted to users. <br><br>\r\nBlacklisting is an approach which consists of checking the input data for malicious characters but a more effective approach is whitelisting. Whitelisting consists of only allowing certain characters to be submitted. For example checking if data submitted is alphanumeric and rejecting the request if it is not. You can use an approach like this after data is submitted and then perform a similar approach before data is outputted to the user. ', 'High', 9),
 ('sqli', 'SQL-Injection', 'SQL injection is a technique in which a user submits SQL statements to a web application in an attempt to exploit the database layer of the application. This can be performed using a browser and entering the SQL statements in a form and submitting the form.', 'This is a critical vulnerability to have on a web application and should be addressed immediately. User-controllable data should be validated before any queries are performed on the database using the data. <br><br>\r\nBlacklisting is an approach which consists of checking the input data for malicious characters but a more effective approach is whitelisting. Whitelisting consists of only allowing certain characters to be submitted. For example checking if data submitted is alphanumeric and rejecting the request if it is not. Many libraries exist, such as built-in libraries for programming languages and open-source libraries, which can assist you in preventing this vulnerability. ', 'High', 10),
 ('basqli', 'Broken Authentication using SQL Injection', 'SQL injection is a technique in which a user submits SQL statements to a web application in an attempt to exploit the database layer of the application. This can be performed using a browser and entering the SQL statements in a form and submitting the form.<br><br>\r\nThe login form can be bypassed using a form of SQL Injection that manipulates the SQL query behind the login form so that it will return one or more results. Therefore, a malicious user can login to the web application without the knowledge of valid credentials.', 'This is a critical vulnerability to have on a web application and should be addressed immediately. User-controllable data should be validated before any queries are performed on the database using the data. <br><br>\r\nBlacklisting is an approach which consists of checking the input data for malicious characters but a more effective approach is whitelisting. Whitelisting consists of only allowing certain characters to be submitted. For example checking if data submitted is alphanumeric and rejecting the request if it is not. Many libraries exist, such as built-in libraries for programming languages and open-source libraries, which can assist you in preventing this vulnerability.', 'High', 11);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
